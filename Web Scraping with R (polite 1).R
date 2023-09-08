82064cca005b1c38447076bcbd21f97c2de18aed
John Karanja <119050743+mikaranja@users.noreply.github.com>
2023-09-08 12:05:17 +0300

    Add files via upload

---
+++ Web Scraping with R (polite 1).R
@@ -0,0 +1,23 @@
+#install.packages("rvest")
+#install.packages("dplyr")
+#install.packages("polite")
+
+library(rvest)
+library(dplyr)
+library(polite)
+
+page <- bow("http://quotes.toscrape.com/",
+            user_agent = "analyst",
+            delay = 5,
+            force = FALSE) %>% 
+  scrape()
+
+
+text <- page %>% html_nodes(".text") %>% html_text()
+author <- page %>% html_nodes(".author") %>% html_text()
+
+
+quotes <- data.frame(text, author, stringsAsFactors = FALSE)
+write.csv(quotes, "quotes.csv")
+View(quotes)
+
