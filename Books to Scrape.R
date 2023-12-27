74c28eb6c6737fa0490882a376f68f7407397479
John Karanja <119050743+mikaranja@users.noreply.github.com>
2023-12-27 21:58:51 +0300

    starters: update

--- Books to Scrape.R
+++ Books to Scrape.R
@@ -1,5 +1,5 @@
-library(tidyverse)
 library(rvest)
+library(tidyverse)
 
 
 get_title = function(book_link){
@@ -10,6 +10,14 @@ get_title = function(book_link){
   return(title)
 }
 
+get_desc = function(book_link){
+  book_info <- read_html(book_link)
+  desc <- book_info %>% 
+    html_node("#product_description+ p") %>% 
+    html_text()
+  return(desc)
+}
+
 get_upc = function(book_link){
   book_info <- read_html(book_link)
   upc <- book_info %>% 
@@ -18,28 +26,31 @@ get_upc = function(book_link){
   return(upc)
 }
 
-get_description = function(book_link){
+get_cat = function(book_link){
   book_info <- read_html(book_link)
-  description <- book_info %>% 
-    html_node("#product_description+ p") %>% 
+  cat <- book_info %>% 
+    html_node("li~ li+ li a") %>% 
     html_text()
-  return(description)
+  return(cat)
 }
 
-get_category = function(book_link){
+get_avail = function(book_link){
   book_info <- read_html(book_link)
-  category <- book_info %>% 
-    html_node(".breadcrumb li~ li+ li a") %>% 
-    html_text()
-  return(category)
+  avail <- as.integer(book_info %>% 
+                        html_node("tr:nth-child(6) td") %>% 
+                        html_text() %>%
+                        str_extract("\\d+"))
+  return(avail)
 }
 
-get_availability = function(book_link){
+get_cover = function(book_link){
   book_info <- read_html(book_link)
-  availability <- book_info %>% 
-    html_node(".availability") %>% 
-    html_text()
-  return(availability)
+  cover <- book_info %>% 
+    html_node("img") %>% 
+    html_attr("src") 
+  cover_suffix <- str_replace_all(cover, "\\.{2}/\\.{2}/", "")
+  cover_url <- paste0("https://books.toscrape.com/", cover_suffix)
+  return(cover_url)
 }
 
 
@@ -57,33 +68,36 @@ for (page_result in seq(from=1,to=50)) {
     html_attr("href") %>% 
     paste0("https://books.toscrape.com/catalogue/", .) %>% 
     unique()
-  book_price <- page %>% 
-    html_nodes(".price_color") %>% 
-    html_text()
+  book_price <- as.integer(page %>% 
+                             html_nodes(".price_color") %>% 
+                             html_text() %>% 
+                             unlist() %>% 
+                             str_remove_all("£") %>% 
+                             str_trim())
   book_rating <- page %>% 
     html_nodes(".star-rating") %>% 
     html_attrs() %>% 
     unlist() %>% 
-    str_remove_all(., "star-rating") %>% 
+    str_remove_all("star-rating") %>% 
     str_trim()
-
+  
   book_rating <- as.integer(factor(book_rating, levels = names(rating_mapping)))
   
   
   book_title <- sapply(book_link, FUN = get_title, USE.NAMES = FALSE)
-  book_upc <- sapply(book_link, FUN = get_upc, USE.NAMES = FALSE) 
-  book_description <- sapply(book_link, FUN = get_description, USE.NAMES = FALSE)
-  book_category <- sapply(book_link, FUN = get_category, USE.NAMES = FALSE)  
-  book_availability <- sapply(book_link, FUN = get_availability, USE.NAMES = FALSE)
-
+  book_desc <- sapply(book_link, FUN = get_desc, USE.NAMES = FALSE)
+  book_cat <- sapply(book_link, FUN = get_cat, USE.NAMES = FALSE)
+  book_upc <- sapply(book_link, FUN = get_upc, USE.NAMES = FALSE)
+  book_cover <- sapply(book_link, FUN = get_cover, USE.NAMES = FALSE) 
+  book_avail <- sapply(book_link, FUN = get_avail, USE.NAMES = FALSE)
+  
   
   books_df <- rbind(books_df, data.frame(
-    book_title, book_upc, book_description, book_category, book_rating, book_price,
-    book_availability, book_link, stringsAsFactors = FALSE))  
+    book_title, book_desc, book_cat, book_upc, book_cover, book_rating, book_price,
+    book_avail, book_link, stringsAsFactors = FALSE))  
   
   
   print(paste("Page", page_result))
 }
 
-write.csv(books_df, "Books to Scrape.csv")
-
+write.csv(books_df, "books_to_scrape.csv")
